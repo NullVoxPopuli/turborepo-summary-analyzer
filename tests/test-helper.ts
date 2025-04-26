b940f56c161725bad71cfc2a031656a5b902a232
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-04-26 15:10:07 -0400

    done?

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -3,9 +3,10 @@ import config, { enterTestMode } from '../app/config.ts';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start as qunitStart } from 'ember-qunit';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
 export function start() {
+  setupEmberOnerrorValidation();
   enterTestMode();
   setApplication(Application.create(config.APP));
 
